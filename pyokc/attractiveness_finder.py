f0d454a26be62ac71f3978798607a54295a8343a
Ivan Malison <IvanMalison@gmail.com>
2014-09-05 02:21:25 -0700

    Remove print logging in attractiveness finder.

--- pyokc/attractiveness_finder.py
+++ pyokc/attractiveness_finder.py
@@ -7,9 +7,8 @@ class AttractivenessFinder(object):
     def __init__(self):
         self._session = Session.login()
 
-    def find_attractiveness(self, username, accuracy=10, _lower=0, _higher=10000):
+    def find_attractiveness(self, username, accuracy=200, _lower=0, _higher=10000):
         average = (_higher + _lower)//2
-        print('searching between {0} and {1}'.format(average, _higher))
         if _higher - _lower < accuracy:
             return average
 
@@ -20,8 +19,6 @@ class AttractivenessFinder(object):
                          attractivenvess_max=_higher)
 
         if results:
-            print('found')
             return self.find_attractiveness(username, accuracy, average, _higher)
         else:
-            print('not found')
             return self.find_attractiveness(username, accuracy, _lower, average)
