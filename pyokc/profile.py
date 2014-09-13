80e07e8ecbe44fce97ceb28e2c11602c148bfb79
Ivan Malison <IvanMalison@gmail.com>
2014-09-12 18:50:10 -0700

    Readd old profile stuff.

--- pyokc/profile.py
+++ pyokc/profile.py
@@ -2,6 +2,7 @@ from lxml import html
 
 from . import helpers
 from . import util
+from .objects import Question
 
 
 class Profile(object):
@@ -11,6 +12,8 @@ class Profile(object):
         self._session = session
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+        self._initialize_fillable_traits()
 
     @util.cached_property
     def _profile_response(self):
@@ -42,5 +45,103 @@ class Profile(object):
     def message(self, message, thread_id=None):
         return helpers.MessageSender(self._session).send_message(self.username, message, self.authcode, thread_id)
 
+
+    def _initialize_fillable_traits(self):
+        self.pics = []
+        self.questions = []
+        self.traits = []
+        self.essays = {
+            'self summary': '',
+            'life': '',
+            'good at': '',
+            'first things': '',
+            'favorites': '',
+            'six things': '',
+            'thinking': '',
+            'friday night': '',
+            'private thing': '',
+            'message me if': '',
+            }
+        self.looking_for = {
+            'gentation': '',
+            'ages': '',
+            'near': '',
+            'single': '',
+            'seeking': '',
+            }
+        self.details = {
+            'last online': '',
+            'orientation': '',
+            'ethnicity': '',
+            'height': '',
+            'body type': '',
+            'diet': '',
+            'smokes': '',
+            'drinks': '',
+            'drugs': '',
+            'religion': '',
+            'sign': '',
+            'education': '',
+            'job': '',
+            'income': '',
+            'relationship type': '',
+            'offspring': '',
+            'pets': '',
+            'speaks': '',
+            }
+
+    def update_questions(self):
+        """
+        Update self.questions with Question instances, which contain
+        text, user_answer, and explanation attributes. See
+        the Question class in objects.py for more details. Like
+        User.update_questions(), note that this can take a while due to
+        OKCupid displaying only ten questions on each page, potentially
+        requiring a large number of requests to the server.
+        """
+        keep_going = True
+        question_number = 0
+        while keep_going:
+            questions_data = {
+                'low': 1 + question_number,
+                }
+            get_questions = self._session.post(
+            'http://www.okcupid.com/profile/{0}/questions'.format(self.name),
+            data=questions_data)
+            tree = html.fromstring(get_questions.content.decode('utf8'))
+            next_wrapper = tree.xpath("//li[@class = 'next']")
+            question_wrappers = tree.xpath("//div[contains(@id, 'question_')]")
+            for div in question_wrappers:
+                if not div.attrib['id'][9:].isdigit():
+                    question_wrappers.remove(div)
+            for div in question_wrappers:
+                question_number += 1
+                explanation = ''
+                text = helpers.replace_chars(div.xpath(".//div[@class = 'qtext']/p/text()")[0])
+                user_answer = div.xpath(".//span[contains(@id, 'answer_target_')]/text()")[0].strip()
+                explanation_span = div.xpath(".//span[@class = 'note']")
+                if explanation_span[0].text is not None:
+                    explanation = explanation_span[0].text.strip()
+                self.questions.append(Question(text, user_answer, explanation))
+            if not len(next_wrapper):
+                keep_going = False
+
+    def update_traits(self):
+        """
+        Fill `self.traits` the personality traits of this profile.
+        """
+        get_traits = self._session.get('http://www.okcupid.com/profile/{0}/personality'.format(self.name))
+        tree = html.fromstring(get_traits.content.decode('utf8'))
+        self.traits = tree.xpath("//div[@class = 'pt_row']//label/text()")
+
+    def update_pics(self):
+        """
+        Fill `self.pics` with url strings of pictures for this profile.
+        """
+        pics_request = self._session.get('http://www.okcupid.com/profile/{0}/photos?cf=profile'.format(self.name))
+        pics_tree = html.fromstring(pics_request.content.decode('utf8'))
+        self.pics = pics_tree.xpath("//div[@id = 'album_0']//img/@src")
+
     def __repr__(self):
         return 'Profile("{0}")'.format(self.username)
+
