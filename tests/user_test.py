0b2edaf5df50d4a1ec52432df3b0acad7c9e7767
Ivan Malison <IvanMalison@gmail.com>
2014-09-26 18:32:04 -0700

    Fix skipif expresssion.

--- tests/user_test.py
+++ tests/user_test.py
@@ -67,7 +67,7 @@ def test_visitors():
     assert isinstance(user.visitors[0], Profile)
 
 
-@pytest.mark.skipif(os.environ.get('CI'), reason="Unicode issues...")
+@pytest.mark.skipif(bool(os.environ.get('CI')), reason="Unicode issues...")
 @util.use_cassette(cassette_name='profile_titles')
 def test_profile_titles():
     user = User()
