a5ab882e5a0f0b3f4ccd6873558e7af818435e95
Ivan Malison <IvanMalison@gmail.com>
2014-10-05 00:43:47 -0700

    Rerecord failing tests, add task to rerecord failing tests.

--- tasks.py
+++ tasks.py
@@ -10,3 +10,11 @@ def pypi():
 def rerecord(rest=''):
     run('tox -e py27 -- --record --credentials test_credentials {0}'.format(rest))
     run('tox -e py27 -- --resave --scrub --credentials test_credentials {0}'.format(rest))
+
+
+@task
+def rerecord_failing():
+    result = run("tox -e py27 | grep test_ | grep \u2015 | sed 's:\\\u2015::g'",
+                 hide='out')
+    for test_name in result.stdout.split('\n'):
+        rerecord(rest='-k {0}'.format(test_name.strip()))
