7db3a72719a3b5b3e691832073ae4b8a2c3c0969
Ivan Malison <IvanMalison@gmail.com>
2014-10-09 19:40:42 -0700

    Use pty for rerecord tasks.

--- tasks.py
+++ tasks.py
@@ -22,10 +22,19 @@ def pypi():
 @ns.add_task
 @task(aliases='r')
 def rerecord(rest):
+    run('tox -e py27 -- --record --credentials test_credentials {0} -s'
+        .format(rest), pty=True)
+    run('tox -e py27 -- --resave --scrub --credentials test_credentials {0} -s'
+        .format(rest), pty=True)
+
+
+@ns.add_task
+@task(aliases='r1')
+def rerecord_one(rest):
     run('tox -e py27 -- --record --credentials test_credentials -k {0} -s'
-        .format(rest))
+        .format(rest), pty=True)
     run('tox -e py27 -- --resave --scrub --credentials test_credentials -k {0} -s'
-        .format(rest))
+        .format(rest), pty=True)
 
 
 @ns.add_task
@@ -34,4 +43,4 @@ def rerecord_failing():
     result = run("tox -e py27 | grep test_ | grep \u2015 | sed 's:\\\u2015::g'",
                  hide='out')
     for test_name in result.stdout.split('\n'):
-        rerecord(rest='-k {0}'.format(test_name.strip()))
+        rerecord_one(rest=test_name.strip())
