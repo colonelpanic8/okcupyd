5b48b6d48427155dfb059f3adb3104f50a067122
Ivan Malison <IvanMalison@gmail.com>
2014-10-12 22:25:41 -0700

    Fix create-githook-symlinks

--- bin/create-githook-symlinks.sh
+++ bin/create-githook-symlinks.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # assuming the script is in a bin directory, one level into the repo
 TOP_LEVEL="$(git rev-parse --show-toplevel)"
-HOOK_DIR="${TOP_LEVEL}""/.git/hooks"
-SOURCE_DIR="$(dirname $BASH_SOURCE)""/git-hooks"
+HOOK_DIR="$TOP_LEVEL""/.git/hooks"
+SOURCE_DIR="$TOP_LEVEL""/bin/git-hooks"
+echo $SOURCE_DIR
 
 for hook in $SOURCE_DIR/*; do
-    # If the hook already exists, is executable, and is not a symlink
-    if [ ! -h $HOOK_DIR/$hook -a -x $HOOK_DIR/$hook ]; then
-        mv $HOOK_DIR/$hook $HOOK_DIR/$hook.local
-    fi
-    source="$hook"
+    source="$SOURCE_DIR/""$(basename $hook)"
     dest="$HOOK_DIR/$(basename $hook)"
-    echo "linking ""$dest""to ""$source"
-    ln -s  "$hook" "$HOOK_DIR/$(basename $hook)"
+    # If the hook already exists, is executable, and is not a symlink
+    if [ -e $dest ]; then
+        mv $dest "$dest.local"
+    fi
+    echo "linking ""$source"" to ""$dest"
+    ln -s  "$source" "$dest"
 done
