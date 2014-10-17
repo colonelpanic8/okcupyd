2d08dc68bddc69f294e3a495ad356c59a7d50c73
Ivan Malison <IvanMalison@gmail.com>
2014-10-17 04:46:12 -0700

    Make warnings show up in red.

--- okcupyd/util/misc.py
+++ okcupyd/util/misc.py
@@ -25,7 +25,7 @@ headers = {
 
 def enable_logger(log_name, level=logging.DEBUG):
     log = logging.getLogger(log_name)
-    handler = ColoredStreamHandler()
+    handler = ColoredStreamHandler(severity_to_style={'WARNING': dict(color='red')})
     handler.setLevel(level)
     log.setLevel(level)
     log.addHandler(handler)
