57d1bdef0163eb5003715942170904ac37a597c0
Ivan Malison <IvanMalison@gmail.com>
2014-10-18 01:48:30 -0700

    Fix stuff.

--- message_handler.py
+++ message_handler.py
@@ -15,28 +15,31 @@ class MessageHandler(object):
     def handle_messages(self, sleep_time=30, sync_until=None):
         while True:
             _, new_messages = mailbox.Sync(self.user).inbox()
-            with txn() as session:
-                for message in new_messages:
-                    session.add(message)
-                for handler in self.handlers:
-                    handler(self.user, new_messages)
+            for handler in self.handlers:
+                handler(self.user, new_messages)
             time.sleep(sleep_time)
 
 
-def do_it():
-    def handle(user, messages):
-        if len(messages) == 0:
-            return
-        messages = messages[::-1]
-        new_wall = u'\n'.join(
-            u'<a href="http://www.okcupid.com/profile/{0}">{0}</a>: {1}\n'.format(
-                message.sender.handle, message.text
-            )
-            for message in messages
-            if message.sender.handle != 'pumpkinmania8'
-        ) + user.profile.essays.my_life
-        user.profile.essays.my_life = new_wall.replace('\n\n', '\n')
+def handle(user, messages):
+    if len(messages) == 0:
+        return
+    messages = messages[::-1]
+    new_wall = u'\n'.join(
+        u'<a href="http://www.okcupid.com/profile/{0}">{0}</a>: {1}\n'.format(
+            message.sender.handle, message.text
+        )
+        for message in messages
+        if message.sender.handle != 'pumpkinmania8'
+    )
+    print new_wall
+    user.profile.essays.my_life = new_wall.replace('\n\n', '\n')
 
+def reset(user, ignore):
+    with txn() as session:
+        messages = model.Message.query_no_txn(session)
+        handle(user, sorted(messages, key=lambda m: m.created_at))
+
+def do_it():
     import okcupyd
     MessageHandler(okcupyd.User.from_credentials('pumpkinmania8', 'thisisforokc'),
-                   handle).handle_messages()
+                   reset).handle_messages(sleep_time=30)
