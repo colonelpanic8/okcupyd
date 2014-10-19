bbde711edc6cceb005385df00520461bacc9358d
Ivan Malison <IvanMalison@gmail.com>
2014-10-19 16:36:13 -0700

    oops..

--- okcupyd/db/mailbox.py
+++ okcupyd/db/mailbox.py
@@ -5,6 +5,7 @@ import simplejson
 from okcupyd import helpers
 from okcupyd.db import adapters
 from okcupyd.db import model, txn
+from okcupyd.util import curry
 
 
 log = logging.getLogger(__name__)
@@ -15,72 +16,37 @@ class Sync(object):
     def __init__(self, user):
         self._user = user
 
-    def all(self, sync_until=None):
+    def all(self):
+        self.update_mailbox('outbox')
+        return self.update_mailbox('inbox')
+
+    @curry
+    def update_mailbox(self, mailbox_name='inbox'):
         with txn() as session:
+            last_updated_name = '{0}_last_updated'.format(mailbox_name)
             okcupyd_user = session.query(model.OKCupydUser).join(model.User).filter(
                 model.User.okc_id == self._user.profile.id
             ).with_for_update().one()
-
             log.info(simplejson.dumps({
-                'outbox_last_updated': helpers.datetime_to_string(
-                    okcupyd_user.outbox_last_updated
-                ),
-                'inbox_last_updated': helpers.datetime_to_string(
-                    okcupyd_user.inbox_last_updated
+                '{0}_last_updated'.format(mailbox_name): helpers.datetime_to_string(
+                    getattr(okcupyd_user, last_updated_name)
                 )
             }))
 
-            okcupyd_user.outbox_last_updated = self._sync_mailbox_until(
-                self._user.outbox(),
-                sync_until or okcupyd_user.outbox_last_updated
-            )
-            okcupyd_user.inbox_last_updated = self._sync_mailbox_until(
-                self._user.inbox(),
-                sync_until or okcupyd_user.inbox_last_updated
-            )
-
-    def inbox(self):
-        with txn() as session:
-            okcupyd_user = session.query(model.OKCupydUser).join(model.User).filter(
-                model.User.okc_id == self._user.profile.id
-            ).with_for_update().one()
-
-            log.info(simplejson.dumps({
-                'inbox_last_updated': helpers.datetime_to_string(
-                    okcupyd_user.inbox_last_updated
-                )
-            }))
-            inbox_last_updated, threads, new_messages = (
-                self._sync_mailbox_until(
-                    self._user.inbox(),
-                    okcupyd_user.inbox_last_updated
-                )
-            )
-            if inbox_last_updated:
-                okcupyd_user.inbox_last_updated = inbox_last_updated
-        return threads, new_messages
-
-    def outbox(self):
-        with txn() as session:
-            okcupyd_user = session.query(model.OKCupydUser).join(model.User).filter(
-                model.User.okc_id == self._user.profile.id
-            ).with_for_update().one()
-
-            log.info(simplejson.dumps({
-                'inbox_last_updated': helpers.datetime_to_string(
-                    okcupyd_user.inbox_last_updated
-                )
-            }))
             res = self._sync_mailbox_until(
                 self._user.outbox(),
                 okcupyd_user.outbox_last_updated
             )
             if not res:
-                return
-            res = outbox_last_updated, threads, new_messages
-            if outbox_last_updated:
-                okcupyd_user.outbox_last_updated = outbox_last_updated
-        return threads, new_messages
+                return None, None
+
+            last_updated, threads, new_messages = res
+            if last_updated:
+                setattr(okcupyd_user, last_updated_name, last_updated)
+            return threads, new_messages
+
+    inbox = update_mailbox(mailbox_name='inbox')
+    outbox = update_mailbox(mailbox_name='outbox')
 
     def _sync_mailbox_until(self, mailbox, sync_until):
         threads = []
