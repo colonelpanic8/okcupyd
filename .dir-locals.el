3a4f7cf5abf749a7db176e3da28cf04bd87733c5
Ivan Malison <IvanMalison@gmail.com>
2014-11-07 14:05:56 -0800

    Remove ffip dir-locals.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,2 @@
-((nil . ((ffip-prune-patterns . (".tox" ".git" "pip" "__pycache__"
-                                 "*.egg-info" "build"))
-         (ffip-patterns . ("*.py" "*.rst" "*.yaml"))
-         (pytest-global-name . "tox -e py27 --")
-         (pytest-cmd-flags . "-sx --enable-logger=okcupyd --enable-logger=requests"))))
+((nil . ((pytest-global-name . "tox -e py27 --")
+	 (pytest-cmd-flags . "-sx --enable-logger=okcupyd --enable-logger=requests"))))
