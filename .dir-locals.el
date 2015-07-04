e5406a1fc66fcb0e559fa566bf48b57cad0393a7
Ivan Malison <imalison@uber.com>
2015-07-03 21:12:57 -0700

    change pytest-global-name

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,2 @@
-((nil . ((pytest-global-name . "tox --")
-		 (pytest-cmd-flags . "-sx --enable-logger=okcupyd --enable-logger=requests"))))
+((nil . ((pytest-global-name . "tox -e py27 --")
+	 (pytest-cmd-flags . "-sx --enable-logger=okcupyd --enable-logger=requests"))))
