254b81eccda5a03ef3a78d6d39919df6ebaeba99
Ivan Malison <imalison@uber.com>
2015-08-07 02:41:46 -0700

    json_search_test.py

---
+++ tests/json_search_test.py
@@ -0,0 +1,10 @@
+from okcupyd import json_search
+
+from tests import util
+
+
+@util.use_cassette
+def test_basic_json_search():
+    for profile in json_search.SearchFetchable()[:4]:
+        assert profile.username
+        assert profile.details.status
