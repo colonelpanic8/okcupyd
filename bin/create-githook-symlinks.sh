46f4eaa9419098f6c299738ce148af55c64deb64
Ivan Malison <IvanMalison@gmail.com>
2016-08-04 16:38:57 -0700

    Fix flycheck issues in create-githook-symlinks

--- bin/create-githook-symlinks.sh
+++ bin/create-githook-symlinks.sh
@@ -3,16 +3,17 @@
 TOP_LEVEL="$(git rev-parse --show-toplevel)"
 HOOK_DIR="$TOP_LEVEL""/.git/hooks"
 SOURCE_DIR="$TOP_LEVEL""/bin/git-hooks"
-echo $SOURCE_DIR
+echo "$SOURCE_DIR"
 
 for hook in $SOURCE_DIR/*; do
-    source="$SOURCE_DIR/""$(basename $hook)"
-    dest="$HOOK_DIR/$(basename $hook)"
+    filename=$(basename "$hook")
+    source="$SOURCE_DIR/$filename"
+    dest="$HOOK_DIR/$filename"
     # If the hook already exists, is executable, and is not a symlink
-    if [ -e $dest ]; then
-        mv $dest "$dest.local"
+    if [ -e "$dest" ]; then
+        mv "$dest" "$dest.local"
     fi
     echo "linking ""$source"" to ""$dest"
     ln -s  "$source" "$dest"
-    chmod 755 $dest
+    chmod 755 "$dest"
 done
